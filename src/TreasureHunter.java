4c7d6b30eb712c98448faf0480e29406d6d66782
michellel314 <michelle.lee.087@gmail.com>
2025-01-15 12:56:46 -0500

    Commit for TH 10 BUG

--- src/TreasureHunter.java
+++ src/TreasureHunter.java
@@ -76,15 +76,15 @@ public class TreasureHunter {
      * Creates a new town and adds the Hunter to it.
      */
     private void enterTown() {
-        double markdown = 0.25;
+        double markdown = 0.5;
         double toughness = 0.4;
         if(easyMode){
-            markdown = 0;
+            markdown = 1;
             toughness = 0.1;
         }
         if (hardMode) {
             // in hard mode, you get less money back when you sell items
-            markdown = 0.5;
+            markdown = 0.25;
 
             // and the town is "tougher"
             toughness = 0.75;
